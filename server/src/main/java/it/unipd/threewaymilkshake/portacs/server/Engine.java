0751a8f8153d414b656ce5a3ddc1b0428cc44ac0
Nicolo Greggio <nicolo.greggio.1@studenti.unipd.it>
2021-03-25 10:53:26 +0100

    fixed collision exception

--- server/src/main/java/it/unipd/threewaymilkshake/portacs/server/Engine.java
+++ server/src/main/java/it/unipd/threewaymilkshake/portacs/server/Engine.java
@@ -63,7 +63,7 @@ class Engine implements Runnable {
       }
 
       try {
-        Thread.sleep(5000);
+        Thread.sleep(1000);
       } catch (InterruptedException e) {
         e.printStackTrace();
       }
